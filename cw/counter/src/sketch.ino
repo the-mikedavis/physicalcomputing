38b8d0a236b243a12af62e0132f0db9356cc3675
Michael Davis <mcd164@miami.edu>
2018-02-12 12:27:50 -0500

    Add functionality of LED

--- cw/counter/src/sketch.ino
+++ cw/counter/src/sketch.ino
@@ -1,4 +1,5 @@
 #define SWITCH 4
+#define LED 3
 
 boolean pressing = false;
 int counter = 0;
@@ -6,6 +7,7 @@ int counter = 0;
 void setup() {
   Serial.begin(9600);
   pinMode(SWITCH, INPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
@@ -14,10 +16,19 @@ void loop() {
   if (switchRead == 1 && !pressing) {
     pressing = true;
     counter++;
+    Serial.print("Click: ");
+    Serial.println(counter);
   } else if (switchRead == 0 && pressing)
     pressing = false;
 
-  Serial.print(pressing ? "true" : "false");
-  Serial.print(", counter: ");
-  Serial.println(counter);
+  if (counter % 3 == 0) {
+    digitalWrite(LED, LOW);
+  } else if (counter % 3 == 1) {
+    digitalWrite(LED, HIGH);
+  } else if (counter % 3 == 2) {
+    digitalWrite(LED, HIGH);
+    delay(500);
+    digitalWrite(LED, LOW);
+    delay(500);
+  }
 }
